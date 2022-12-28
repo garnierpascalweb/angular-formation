d6857b97965b5ca5af40002b846d49afaec7ee40
Garnier <pascalscn@hotmail.com>
2022-12-28 20:09:29 +0100

    36:45 44:45 - Les directves structurelles : ngIf, ngFor

--- mon-projet-angular/src/app/app.component.ts
+++ mon-projet-angular/src/app/app.component.ts
@@ -11,9 +11,28 @@ export class AppComponent {
    * Booleen - est authentifié
    */
   isAuth = false;
-  appareilOne = 'MAchine a laver du ts';
-  appareilTwo = 'Télévision du ts';
-  appareilThree = 'Ordinateur du ts';
+  //appareilOne = 'MAchine a laver du ts';
+  //appareilTwo = 'Télévision du ts';
+  //appareilThree = 'Ordinateur du ts';
+
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'allumé'
+    },
+    {
+      name: 'Télévision',
+      status: 'éteint'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'allumé'
+    },
+    {
+      name: 'Climatisation',
+      status: 'allumé'
+    },
+  ]
 
   /**
    * Executé au moment de la cration du component
