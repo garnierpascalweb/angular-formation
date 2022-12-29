4c0cea170c298d174c5053e3b5f67d9312d9b343
Garnier <pascalscn@hotmail.com>
2022-12-29 10:44:16 +0100

    1:17:00 1:18:30 - demenagement dans appareilview

--- mon-projet-angular/src/app/app.component.ts
+++ mon-projet-angular/src/app/app.component.ts
@@ -7,60 +7,8 @@ import { AppareilService } from './services/appareil.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'my awesone app';
-  
-  /**
-   * Booleen - est authentifié
-   */
-  isAuth = false;
-  //appareilOne = 'MAchine a laver du ts';
-  //appareilTwo = 'Télévision du ts';
-  //appareilThree = 'Ordinateur du ts';
-
-  // lastUpdate = new Date();
-  
-  lastUpdate = new Promise<Date>(
-    (resolve, reject) => {
-      const date = new Date();
-      setTimeout(
-        () => {
-          resolve(date);
-        }, 5000
-      );
-    }
-  )
-  
-  appareils: any[];
-
-  /**
-   * Executé au moment de la cration du component
-   * Injection de appareilService, du coup on peut l'appeler de partout via this.appareilService
-   */
-  constructor( private appareilService : AppareilService ){
-    this.appareils = [];
-    setTimeout (
-      () => {
-        this.isAuth  = true
-      }, 4000
-    )
+export class AppComponent  {
+  constructor(){
+    
   }
-  
-  /**
-   * executee au moment de la creation du component par Angular
-   * et apres l'execution du constructor
-   */
-  ngOnInit(): void {
-    this.appareils = this.appareilService.appareils;
-  }
-;
-
-  onAllumer(){
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre(){
-    this.appareilService.switchOffAll();
-  }
-
 }
--- mon-projet-angular/src/app/appareil-view/appareil-view.component.ts
+++ mon-projet-angular/src/app/appareil-view/appareil-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppareilService } from '../services/appareil.service';
 
 @Component({
   selector: 'app-appareil-view',
@@ -7,9 +8,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppareilViewComponent implements OnInit {
 
-  constructor() { }
+  title = 'my awesone app';
+  
+  /**
+   * Booleen - est authentifié
+   */
+  isAuth = false;
+  //appareilOne = 'MAchine a laver du ts';
+  //appareilTwo = 'Télévision du ts';
+  //appareilThree = 'Ordinateur du ts';
 
+  // lastUpdate = new Date();
+  
+  lastUpdate = new Promise<Date>(
+    (resolve, reject) => {
+      const date = new Date();
+      setTimeout(
+        () => {
+          resolve(date);
+        }, 5000
+      );
+    }
+  )
+  
+  appareils: any[];
+
+  /**
+   * Executé au moment de la cration du component
+   * Injection de appareilService, du coup on peut l'appeler de partout via this.appareilService
+   */
+  constructor( private appareilService : AppareilService ){
+    this.appareils = [];
+    setTimeout (
+      () => {
+        this.isAuth  = true
+      }, 4000
+    )
+  }
+  
+  /**
+   * executee au moment de la creation du component par Angular
+   * et apres l'execution du constructor
+   */
   ngOnInit(): void {
+    this.appareils = this.appareilService.appareils;
+  }
+;
+
+  /**
+   * Permet d'allumer tous les appareils
+   */
+  onAllumer(){
+    this.appareilService.switchOnAll();
+  }
+
+  /**
+   * Permet d'eteindre tous les appareils
+   */
+  onEteindre(){
+    this.appareilService.switchOffAll();
   }
 
 }
